f378ba997ac8d8eddb0ecd0167f0d68fb6252b01
ayuka <basketball110512@gmail.com>
2024-09-03 05:28:01 +0900

    confirm.css、blade.phpファイル名つづり誤りにつき修正

---
+++ src/resources/views/auth/confirm.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/comfirm.css') }}">
+@endsection
+
+@section('content')
+<div class="confirm">
+    <div class="confirm-form">
+        <h2 class="confirm-txt">会員登録ありがとうございます</h2>
+        <form class="confirm__form-area" action="/login" method="get">
+            @csrf
+            <button class="confirm__form-btn" type="submit">
+                <div class="confirm__form-btn-txt">ログインする</div>
+            </button>
+        </form>
+    </div>
+</div>
+@endsection
